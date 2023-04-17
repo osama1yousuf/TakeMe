5a95c51e051ed89319135bd247abc7cfc38a967f
osama_yousuf <dev.eusopht2@gmail.com>
2023-04-18 03:46:32 +0500

    sixth commit

---
+++ src/element/Caresoul.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const Caresoul = ({children})=>{
+   return(
+    <div className=" relative" >
+        <div className="">{children}</div>
+    </div>
+   )   
+}
+
+export default Caresoul
--- src/pages/SallerCategary.js
+++ src/pages/SallerCategary.js
@@ -4,29 +4,24 @@ import Header from "../component/header";
 import { BsFillStarFill } from "react-icons/bs";
 import { useDispatch } from "react-redux";
 import { appInfoAction } from "../store/Slice/appInfoSlice";
-import Carousel from "react-bootstrap/Carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from "react-responsive-carousel";
-
+import Caresoul from "../element/Caresoul";
+import {BsFillCaretDownFill} from "react-icons/bs";
 const SallerCategary = () => {
   const dispatch = useDispatch();
   const [data, setData] = useState([]);
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === data.length - 1 ? 0 : prevSlide + 1
-    );
-  };
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? data.length - 1 : prevSlide - 1
-    );
-  };
+  const [selectedId  , setSelectedId] = useState(null) 
   useEffect(() => {
     dispatch(appInfoAction.sideBar(false));
+    console.log(productSaller);
     setData(productSaller);
   }, []);
+  const handleShow =(e)=>{
+    if (selectedId === null) {
+      setSelectedId(e.id)
+    }else{
+      setSelectedId(null)
+    }
+  }
   return (
     <di>
       <Header>
@@ -35,31 +30,57 @@ const SallerCategary = () => {
       <div className="bg-gray-200">
         {data
           ? data.map((val, ind) => {
-              return (
-                <div
-                  className="aligin-center mt-2 p-2 bg-white"
-                  style={{ direction: "rtl" }}
-                >
-                  <div className="flex ">
-                    <div className="w-14 m-3">
-                      <img src={val.imagePath} />
-                    </div>
-                    <div>
-                      <h1 className="font-black text-lg">{val.name}</h1>
-                      <h3 className="text-sm">{val.city}</h3>
-                      <p className="text-xs">{val.productsCountMsg}</p>
-                      <div className="flex gap-2 mt-3">
-                        <BsFillStarFill style={{ color: "#02ac84" }} />
-                        <h3 style={{ color: "#02ac84" }} className="text-sm">
-                          {val.ratingMsg}
-                        </h3>
-                      </div>
+            return (
+              <div
+                className="aligin-center mt-2 p-2 bg-white"
+                style={{ direction: "rtl" }}
+              >
+                <div className="flex ">
+                  <div className="w-14 m-3">
+                    <img src={val.imagePath} />
+                  </div>
+                  <div>
+                    <h1 className="font-black text-lg">{val.name}</h1>
+                    <h3 className="text-sm">{val.city}</h3>
+                    <p className="text-xs">{val.productsCountMsg}</p>
+                    <div className="flex gap-2 mt-3">
+                      <BsFillStarFill style={{ color: "#02ac84" }} />
+                      <h3 style={{ color: "#02ac84" }} className="text-sm">
+                        {val.ratingMsg}
+                      </h3>
                     </div>
                   </div>
-                  <div></div>
                 </div>
-              );
-            })
+             <div className="max-w-lg">
+              <Caresoul>
+                {val.products[1].map((newVal , i)=>{
+                    return(
+                        <div style={{width:"full"}}>
+                          <div className="flex item-center mt-3">
+                          <img src={newVal.images[0].imagePath} alt={newVal.name} />
+                          </div>
+                          <div  className="flex justify-between mt-4">
+                            <h1 className="font-semibold text-2xl">{newVal.name}</h1>
+                           < BsFillCaretDownFill size={"20px"} onClick={(e)=> handleShow(newVal)} className="mx-5 mt-2" style={{color:"#02ac84"}} />
+                          </div>
+                          <div className="text-sm p-3" style={{
+                            display:newVal.id === selectedId ? "block" : "none"
+                          }}  dangerouslySetInnerHTML={{__html:newVal.description.replace(/<div>\n<img-key>(.*?)<\/img-key>/g, '<div class="flex">\n<img class="ml-3" width="20px" height="auto" src="$1"/>')}}>
+                            
+                          </div>
+                          <div className="mt-2">
+                            <h2 className="font-thin text-lg" >{newVal.saleDetails.title}</h2>
+                            <h2 className="font-hairline text-sm">{newVal.saleDetails.priceMsg}</h2>
+                            <h2 className="font-hairline text-sm mt-2"><span style={{color:"#02ac84"}}> **  </span>{newVal.saleDetails.statusMsg}</h2>
+                          </div>
+                        </div>
+                    )
+                })}
+              </Caresoul>
+             </div>
+              </div>
+            );
+          })
           : ""}
       </div>
     </di>
